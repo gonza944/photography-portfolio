ad9dd668e4f2bb290a315c99da76cc1446e13d74
Gonzalo Ariza Sabbatini <gonza94.4@gmail.com>
2024-01-09 16:54:41 -0300

    Build-home-skelleton
    * Basic structure and font selection
    
    * Add a simple navBar

---
+++ const.ts
@@ -0,0 +1,19 @@
+// Primary Tokens
+export const designSystemColors = {
+ gray: '#F2F2F2',
+ gray2: '#2B2B2B',
+ dough: '#F7EBD5',
+ dirt: '#D9CDB8',
+ orange1: '#F28374',
+}
+
+
+// Functional Tokens
+
+export const functionalTokens = {
+    backgroundColor: designSystemColors.gray,
+    primaryColor: designSystemColors.orange1,
+    fontColor: designSystemColors.gray2,
+    grainColor: designSystemColors.dough,
+    secondaryColor: designSystemColors.dirt,
+}
---
+++ src/components/Landing/landing.tsx
@@ -0,0 +1,8 @@
+const Landing = () => {
+  return (
+    <div className="font-serif">
+    </div>
+  );
+};
+
+export default Landing;
