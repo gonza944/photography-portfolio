9ec131e6250e3a6e1d7f02f42b0e3f66e674a1d2
Gonzalo Ariza Sabbatini <gonza94.4@gmail.com>
2024-01-17 16:41:36 -0300

    Revert "Connect with the Blob store" (#5)

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: '4owlxcrlkmcznxj4.public.blob.vercel-storage.com',
-          port: '',
-          pathname: '/Portfolio/**',
-        },
-      ],
-    },
-  }
+const nextConfig = {}
 
 module.exports = nextConfig
