aecbc33095bc44a137e42405c81df52e57f75e2f
Gonzalo Ariza Sabbatini <Gonza94@gmail.com>
2024-02-29 17:30:24 -0300

    Add: photo background information for desktop

--- src/components/Infinite-grid/infinite-grid.tsx
+++ src/components/Infinite-grid/infinite-grid.tsx
@@ -41,10 +41,10 @@ const InfiniteGrid: React.FC<{ elements: Random[] }> = ({ elements }) => {
   return (
     <div className="grid grid-flow-row grid-cols-3 gap-8 max-lg:grid-cols-2 max-sm:grid-cols-1">
       {photos.map((element, index) => (
-        <div key={index}>
-          <Link href={`/photoModal/${element.id}`}>
+        <div key={index}  className="relative">
+          <Link href={`/photoModal/${element.id}`}  className="relative">
             <Image
-              className=" rounded-md shadow-md shadow-fontColor max-sm:rounded-none"
+              className="relative z-20 rounded-md shadow-md shadow-fontColor max-sm:rounded-none max-sm:filter-none hover:z-0"
               ref={
                 index === elements.length - percentileToFetchNewData
                   ? targetRef
@@ -56,6 +56,11 @@ const InfiniteGrid: React.FC<{ elements: Random[] }> = ({ elements }) => {
               alt={element.alt_description || "Image"}
               style={{ width: "auto", height: "auto" }}
             />
+            {element.description && (
+              <div className="absolute z-10 hover:z-30 rounded-md max-sm:rounded-none bg-gradient-to-t from-stone-300 to-transparent w-[100%] h-[100%] bottom-0 p-4 pt-32 pb-8 flex items-end justify-center">
+                <p>{element.description}</p>
+              </div>
+            )}
           </Link>
         </div>
       ))}
