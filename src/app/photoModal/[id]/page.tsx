c52230eba803f3a8dfc86b089e813270e0c3fa26
Gonzalo Ariza Sabbatini <Gonza94@gmail.com>
2024-06-20 11:51:01 -0300

    Fix: deploy build

--- src/app/photoModal/[id]/page.tsx
+++ src/app/photoModal/[id]/page.tsx
@@ -2,12 +2,6 @@ import { PhotoProps } from "@/app/@modal/(.)photoModal/[id]/page";
 import { getPhotoByIdServer } from "@/app/actions";
 import Displayer from "@/components/Photo-Displayer/photoDisplayer";
 
-export const getMetadata = ({params}: PhotoProps) => {
-  return {
-    title: `Photo ${params.id}`,
-  };
-}
-
 const PhotoModal: React.FC<PhotoProps> = async ({ params }) => {
   const photo = await getPhotoByIdServer(params.id);
 
