5757c4c2542d8d1370168543e67e189a25e50e5d
Gonzalo Ariza Sabbatini <Gonza94@gmail.com>
2024-06-26 12:10:18 -0300

    Fix: image loader not working

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,9 +1,6 @@
-"use server";
-
 import { createShuffledArray } from "@/utils/utils";
 import { env } from "process";
 import { cache } from "react";
-import { createApi } from "unsplash-js";
 import { Nullable } from "unsplash-js/dist/helpers/typescript";
 
 export type Photo = {
@@ -21,56 +18,26 @@ export const getNewPhotosRequest = cache(
     startFrom: number = 1
   ) => {
     try {
-      if (env.NODE_ENV === "development") {
-        const unsplashClient = createApi({
-          accessKey: `${env.UNSPLASH_API_KEY}`,
-        });
+      const now = new Date();
+      const indexsToFetch = createShuffledArray(
+        Number.parseInt(env.NEXT_PUBLIC_INFINITY_GRID_LENGTH || "82"),
+        now.getMonth()
+      );
+      const numberOfPhotosInt = Number.parseInt(numberOfPhotos);
 
-        const data = await unsplashClient.photos
-          .getRandom({
-            count: Number.parseInt(numberOfPhotos),
-          })
-          .then((data) => {
-            const { response } = data;
-            if (response instanceof Array) {
-              return response?.map((photo) => ({
-                width: photo.width,
-                height: photo.height,
-                alt: photo.alt_description,
-                src: photo.urls.regular,
-                id: photo.id,
-              }));
-            } else {
-              return [
-                {
-                  width: response?.width,
-                  height: response?.height,
-                  alt: response?.alt_description,
-                  src: response?.urls.regular,
-                  id: response?.id,
-                },
-              ];
-            }
-          });
-
-        return data as Photo[];
-      } else {
-        const now = new Date();
-        const indexsToFetch = createShuffledArray(
-          Number.parseInt(env.NEXT_PUBLIC_INFINITY_GRID_LENGTH || "82"),
-          now.getMonth()
-        );
-        const numberOfPhotosInt = Number.parseInt(numberOfPhotos);
-
-        const photos = indexsToFetch.slice(startFrom + 1, startFrom + numberOfPhotosInt + 1).map((photoId) => ({
+      const photos = indexsToFetch
+        .slice(startFrom + 1, startFrom + numberOfPhotosInt + 1)
+        .map((photoId) => ({
           width: 600,
           height: 900,
           alt: "Portfolio Photo",
-          src: `${env.NEXT_PUBLIC_IMGIX_DOMAIN}/Portfolio-${photoId}.jpg?auto=format&fit=cropw=600`,
+          src: `${
+            env.NEXT_PUBLIC_IMGIX_DOMAIN ||
+            "https://gonzaloariza-975314016.imgix.net"
+          }/Portfolio-${photoId}.jpg?auto=format&fit=cropw=600`,
           id: `Portfolio-${photoId}`,
         }));
-        return photos;
-      }
+      return photos;
     } catch (error) {
       throw new Error("failed to fetch new photos");
     }
@@ -79,31 +46,16 @@ export const getNewPhotosRequest = cache(
 
 export const getPhotoById = cache(async (id: string) => {
   try {
-    if (env.NODE_ENV === "development") {
-      const unsplashClient = createApi({
-        accessKey: `${env.UNSPLASH_API_KEY}`,
-      });
-
-      const data = await unsplashClient.photos
-        .get({ photoId: id })
-        .then((data) => data.response);
-
-      return {
-        width: data?.width || 600,
-        height: data?.height || 900,
-        alt: data?.alt_description || "",
-        src: data?.urls.regular || "",
-        id: data?.id,
-      };
-    } else {
-      return {
-        width: 1080,
-        height: 1900,
-        alt: "Portfolio Photo",
-        src: `${env.NEXT_PUBLIC_IMGIX_DOMAIN}/${id}.jpg`,
-        id: id,
-      };
-    }
+    return {
+      width: 1080,
+      height: 1900,
+      alt: "Portfolio Photo",
+      src: `${
+        env.NEXT_PUBLIC_IMGIX_DOMAIN ||
+        "https://gonzaloariza-975314016.imgix.net"
+      }/${id}.jpg`,
+      id: id,
+    };
   } catch (error) {
     throw new Error("failed to fetch new photos");
   }
@@ -113,6 +65,7 @@ export const getPhotoByIdServer = cache(async (id: string) => {
   try {
     const data = await getPhotoById(id);
 
+    //Fix tipying
     return data as Photo;
   } catch (error) {
     throw new Error("failed to fetch new photos");
